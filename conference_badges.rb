8d718089bfd0a07400a39ac0facd5eee0ff11d2f
Mars Wong <marswong618@gmail.com>
2018-01-24 14:24:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,17 +4,18 @@ end
 
 def batch_badge_creator(attendees)
   badges = []
-  
+
   attendees.each { |x| badges << "Hello, my name is #{x}." }
-  
+
   return badges
 end
 
 def assign_rooms(attendees)
   room_assignments = []
-  
+
   attendees.each_index do |x|
+    room_assignments
   end
-  
+
   return room_assignments
 end
